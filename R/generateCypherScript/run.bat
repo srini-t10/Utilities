1ceb53c7e6216c3053aeb2608c6d5d8d53dea2a3
srini.t <srini.t@outlook.com>
2017-05-08 10:43:04 -0700

    Update the readme file and remove the R dll's and libraries to keep (the size of) this utility lite

--- R/generateCypherScript/run.bat
+++ R/generateCypherScript/run.bat
@@ -1,6 +1,5 @@
 :: Set the location RScript.exe in a variable
-:: set RScript="C:\Program Files\R\R-3.3.2\bin\RScript.exe"
-set RScript="R\R-3.3.2\bin\RScript.exe"
+set RScript="C:\Program Files\R\R-3.3.2\bin\RScript.exe"
 
 :: Run the RScript 
 %Rscript% "script_gencypher.r"
