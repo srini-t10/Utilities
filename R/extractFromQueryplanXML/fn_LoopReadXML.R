f6b1e39a56f7143129dbf61ac514caf717969820
srini-t10 <srini.t@outlook.com>
2017-08-20 00:41:00 -0700

    Update fn_LoopReadXML R file to handle Errors

--- R/extractFromQueryplanXML/fn_LoopReadXML.R
+++ R/extractFromQueryplanXML/fn_LoopReadXML.R
@@ -11,7 +11,7 @@
 #
 # Author: srini-t10
 #
-# Version: 0.1
+# Version: 0.2
 #
 #*******************************************************************************************************
 
@@ -21,6 +21,7 @@ fn_LoopReadXML <- function()
 {
   # Load Required packages
   require(xml2)
+  require(stringi)
  
   #set input, output locations
   xmllocation <- "./Input/"
@@ -31,20 +32,73 @@ fn_LoopReadXML <- function()
   
   # Initiate Output variable
   output <- NULL
+  ErrorCount <- 0
+  
+  
   
   # Extract schema from each xml
-  for(i in 1:length(filename)) {
-    xmldoc <- read_xml(paste(xmllocation,filename[i],sep = ""))
-    readschema <- fn_extractSchema(xmldoc)
-    readschema <-cbind(FileName= sub(".xml","",filename[i]), readschema)
-    output <- rbind(output,readschema)
-  }
   
-  names(output) <- c("TableName","SourceDatabase","SourceSchema","SourceTable","SourceTableAlias","SourceColumn")
+  message("")
+  message("Processing XML Files from Input Folder..")
+  
+  for(i in 1:length(filename)) {
+    
+        # Display message
+        message(i,"/",length(filename)," ",filename[i])
+        
+        # extract schema information from XML, execute in a try block
+        tryERROR <- try(
+            {
+            xmldoc <- read_xml(paste(xmllocation,filename[i],sep = ""))
+            readschema <- fn_extractSchema(xmldoc)
+            readschema <-cbind(FileName= sub(".xml","",filename[i]), readschema)
+            }
+        )
+        
+        # Insert Error row if error occured
+        if(class(tryERROR) == "try-error")
+        {
+            ErrorCount <- ErrorCount + 1
+            readschema <-c(sub(".xml","",filename[i]), 
+                           c("ERROR",
+                             "ERROR",
+                             "ERROR", 
+                             geterrmessage()))
+            readschema <-rbind(readschema)  
+        }
+        # if no error, bind the results
+        else if(length(colnames(readschema)) == 6)
+        {
+            readschema <-readschema[,c(1:4,6)]
+        }
+        else if(length(colnames(readschema)) == 5)
+        {
+            readschema <-readschema
+        }
+        else
+        {
+            readschema <-c(sub(".xml","",filename[i]), 
+                           c("Schema Not Found",
+                             "Schema Not Found",
+                             "Schema Not Found", 
+                             "Schema Not Found"))
+            readschema <-rbind(readschema)  
+        }
+        
+        
+        colnames(readschema) <-c("TableName","SourceDatabase","SourceSchema","SourceTable","SourceColumn")
+        
+        # bind the result for each file in an output dataframe
+        output <- rbind(output,readschema)
+        
+      
+  }
   
   write.csv(output,paste(outlocation,"Schema.csv",sep = ""), row.names = F)
   
+  message("")
   message("Execution Successful! schema.csv file is placed in Output folder..")
+  message("Total File Procesed: ",length(filename),", Succesfully Processed: ",length(filename)-ErrorCount,", Not Processed due to XML File Error: ",ErrorCount,"..")
   
 }
 
